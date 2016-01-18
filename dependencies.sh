4ed1ba615df35be5cd023d56bc4f97d4ed3daa3e
RoboQYD <emailqasim@gmail.com>
2016-01-18 02:09:08 -0500

    Attempt 1

---
+++ dependencies.sh
@@ -0,0 +1,9 @@
+i   python-matplotlib
+i A python-matplotlib-data
+i A python-numpy
+    python-pyaudio 
+
+aptitude -f install python-matplotlib
+aptitude -f install python-pyaudio
+aptitude -f install python-numpy
+aptitude -f install python-matplotlib-data
