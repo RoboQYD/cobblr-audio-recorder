c96ae20d477cdbc6bddae8c7f41461206e58bd79
RoboQYD <emailqasim@gmail.com>
2016-01-18 15:21:44 -0500

    Fixed dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,8 +1,3 @@
-i   python-matplotlib
-i A python-matplotlib-data
-i A python-numpy
-    python-pyaudio 
-
 aptitude -f install python-matplotlib
 aptitude -f install python-pyaudio
 aptitude -f install python-numpy
